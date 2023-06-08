f018594e5d6bb029aaf8fc9bee9bfd312d1e48b0
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2023-06-08 22:15:02 +1000

    start capturing input

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿ConsoleKeyInfo key = new();
+CancellationTokenSource cancelSource = new();
+Task watchKeys = Task.Run(() => { WatchKeys(); });
+// int counter = 0;
+
+Console.Clear();
+
+while (true)
+{
+    if (key.Key == ConsoleKey.Q)
+    {
+        Console.WriteLine($"actions: {key.Key}");
+    }
+    else
+    {
+        Console.WriteLine("actions: ");
+    }
+    // Console.WriteLine(counter);
+    // counter++;
+    Console.Clear();
+}
+
+void WatchKeys()
+{
+    do 
+    {
+        key = Console.ReadKey(true);
+    } while (key.Key != ConsoleKey.Q);
+    cancelSource.Cancel();
+}
