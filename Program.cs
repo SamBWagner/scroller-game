bc0e5aa65b5f6f92d64c39b023d58b0b33735270
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2023-06-10 13:56:11 +1000

    Player can now move their cursor left and right

--- Program.cs
+++ Program.cs
@@ -1,41 +1,65 @@
-﻿namespace Game {
-    class Program
+﻿class Program
+{
+    private static char m_playerCharacter = '\u2588';
+    private static char[] m_playerLine = new[] {' ', ' ', ' ', ' ', ' ', ' ', ' '};
+    private static int m_playerPosition = 3;
+
+    private static ConsoleKeyInfo Key = new();
+    private static int REFRESH_RATE = 16;
+    private static bool GameEnded;
+
+    private static void Main(string[] args)
     {
-        public static ConsoleKeyInfo key = new();
-        public static int REFRESH_RATE = 16;
-        public static bool gameEnded;
+        int counter = 0;
+        Console.Clear();
+        var LastKeyThread = new Thread(WatchKeys);
+        LastKeyThread.Start();
 
-        private static void Main(string[] args)
+        var gameThread = new Thread(GameLoop);
+        gameThread.Start();
+
+        void WatchKeys()
         {
-            int counter = 0;
-            GameDrawer drawer = new();
-            Console.Clear();
-            var keyThread = new Thread(WatchKeys);
-            keyThread.Start();
-
-            var gameThread = new Thread(GameLoop);
-            gameThread.Start();
-
-            void WatchKeys()
+            while (!GameEnded)
             {
-                while (!gameEnded)
-                {
-                    key = Console.ReadKey(true);
-                    Thread.Sleep(REFRESH_RATE);
-                }
+                Key = Console.ReadKey(true);
+                Thread.Sleep(REFRESH_RATE);
             }
+        }
 
-            void GameLoop()
+        void GameLoop()
+        {
+            while (Key.Key != ConsoleKey.Q && !GameEnded)
             {
-                while (key.Key != ConsoleKey.Q && !gameEnded)
+                Console.WriteLine($"Key: {Key.Key} | ticks: {counter}");
+
+                if (Key.Key == ConsoleKey.H && m_playerPosition != 0) {
+                    m_playerLine[m_playerPosition] = ' ';
+                    m_playerPosition--;
+                    Key = new();
+                } 
+                else if (Key.Key == ConsoleKey.L && m_playerPosition != 6)
                 {
-                    Console.WriteLine($"action: {key.Key}\nticks: {counter}");
-                    drawer.Draw(3);
-                    Thread.Sleep(REFRESH_RATE);
-                    Console.Clear();
+                    m_playerLine[m_playerPosition] = ' ';
+                    m_playerPosition++;
+                    Key = new();
                 }
-                Program.gameEnded = true;
+
+                Draw(playerPosition: m_playerPosition, width: 7, height: 15);
+                Thread.Sleep(REFRESH_RATE);
+                Console.Clear();
             }
+            GameEnded = true;
+        }
+
+        void Draw(int playerPosition, int width, int height)
+        {
+            for(int i = 0; i < height - 1; i++)
+            {
+                Console.WriteLine();
+            }
+            m_playerLine[playerPosition] = m_playerCharacter;
+            Console.WriteLine(string.Concat(m_playerLine));
         }
     }
 }
