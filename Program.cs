622b336839b8b52b55093220f7eb944079cd5ddd
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2023-06-11 10:34:17 +1000

    game now has persistant world

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
-﻿/* 
- * TODO: Draw the game in full, frame by frame
- * TODO: Build obstacle inserting mechanism
- * TODO: Build game end state
-*/
+﻿// TODO: Build obstacle inserting mechanism
+// TODO: Build game end state
+
+using System.Text;
 
 class Program
 {
@@ -10,9 +9,29 @@ class Program
     private static char[] m_playerLine = new[] {' ', ' ', ' ', ' ', ' ', ' ', ' '};
     private static int m_playerPosition = 3;
 
-    private static ConsoleKeyInfo Key = new();
-    private static int REFRESH_RATE = 16; // ~60fps
-    private static bool GameEnded;
+    private static ConsoleKeyInfo m_Key = new();
+    private static int m_RefreshRate = 16; // ~60fps
+    private static bool m_GameEnded;
+    private static int m_height = 15;
+    private static int m_width = 7;
+    private static char[,] m_gameWorld = new char[15,7] 
+    {
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+        {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    };
 
     private static void Main(string[] args)
     {
@@ -26,45 +45,51 @@ class Program
 
         void WatchKeys()
         {
-            while (Key.Key != ConsoleKey.Q && !GameEnded)
+            while (m_Key.Key != ConsoleKey.Q && !m_GameEnded)
             {
-                Key = Console.ReadKey(true);
+                m_Key = Console.ReadKey(true);
             }
         }
 
         void GameLoop()
         {
-            while (Key.Key != ConsoleKey.Q && !GameEnded)
+            while (m_Key.Key != ConsoleKey.Q && !m_GameEnded)
             {
-                Console.WriteLine($"Key: {Key.Key} | ticks: {counter}");
+                Console.WriteLine($"Key: {m_Key.Key} | ticks: {counter}");
 
-                if (Key.Key == ConsoleKey.H && m_playerPosition != 0) {
+                if (m_Key.Key == ConsoleKey.H && m_playerPosition != 0) {
                     m_playerLine[m_playerPosition] = ' ';
                     m_playerPosition--;
                 } 
-                else if (Key.Key == ConsoleKey.L && m_playerPosition != 6)
+                else if (m_Key.Key == ConsoleKey.L && m_playerPosition != 6)
                 {
                     m_playerLine[m_playerPosition] = ' ';
                     m_playerPosition++;
                 }
-                Key = new();
+                m_Key = new();
+                
+                // Gameworld Drawing Logic
+                StringBuilder builder = new();
+                for (int i = 0; i < m_height; i++)
+                {
+                    builder.Append('|');
+                    for (int j = 0; j < m_width; j++)
+                    {
+                        builder.Append(m_gameWorld[i,j]);
+                    }
+                    builder.Append("|");
+                    Console.WriteLine(builder);
+                    builder.Clear();
+                }
 
-                Draw(playerPosition: m_playerPosition, width: 7, height: 15);
+                m_playerLine[m_playerPosition] = m_playerCharacter;
+                Console.WriteLine("|" + string.Concat(m_playerLine)+ "|");
+
                 counter++;
-                Thread.Sleep(REFRESH_RATE);
+                Thread.Sleep(m_RefreshRate);
                 Console.Clear();
             }
-            GameEnded = true;
-        }
-
-        void Draw(int playerPosition, int width, int height)
-        {
-            for(int i = 0; i < height - 1; i++)
-            {
-                Console.WriteLine("|".PadRight(width + 1, ' ') + "|");
-            }
-            m_playerLine[playerPosition] = m_playerCharacter;
-            Console.WriteLine("|" + string.Concat(m_playerLine)+ "|");
+            m_GameEnded = true;
         }
     }
 }
